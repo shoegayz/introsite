e4568eb2ad080dbb5e33382c189f44df6048c0e5
alex <lordfoogsman@gmail.com>
2023-09-09 15:32:48 -0500

    Initial Commit

---
+++ sparkles.js
@@ -0,0 +1,31 @@
+function createSparkle(){
+    //create a div class for my sparkle!! thanks hazy for helping with this <3
+    const sparkle = document.createElement('div')
+    sparkle.classList.add('sparkle')
+
+    //putting the sparkle at a random spot on the screen!!! 
+    //"left" and "top" are like x and y values on an an axis!!
+    sparkle.style.left = `${Math.random() * (window.innerWidth - 15)}px`
+    sparkle.style.top = `${Math.random() * (window.innerHeight - 15)}px`
+    //hueshift! hue, saturation, lightness!
+    sparkle.style.backgroundColor = `hsl(${Math.random() * 360}deg, 100%, 70%)`
+    //adding it to <body>!
+    document.body.appendChild(sparkle)
+
+    sparkle.addEventListener('animationend', () => {sparkle.remove()
+    })
+    
+    setTimeout(() => sparkle.remove(), 2000)
+}
+
+function createSparkleLoop(){
+    const sparkleCount = Math.random() * 100
+    for (let i = 0; i < sparkleCount; i++) {
+        setTimeout(createSparkle, Math.random() * 200)
+    }
+    setTimeout(createSparkleLoop, Math.random() * 800 + 100)
+
+}
+//repeat sparkle loop :3333
+
+createSparkleLoop()
